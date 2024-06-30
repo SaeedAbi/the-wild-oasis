5490ba6f3df2635fd9c72dcb8b1c58aea3421071
SaeedAbi <abdary.saeed@gmail.com>
2024-06-30 14:09:17 +0330

    :construction: make account component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
-import styled from "styled-components";
-import GlobalStyles from "./styles/GlobalStyles.js";
-import Heading from "./ui/Heading.jsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Dashboard from "./pages/Dashboard.jsx";
+import Bookings from "./pages/Bookings.jsx";
+import Cabins from "./pages/Cabins.jsx";
+import Users from "./pages/Users.jsx";
+import Settings from "./pages/Settings.jsx";
+import Account from "./pages/Account.jsx";
+import Login from "./pages/Login.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
 
+const App = () => {
+    return (
+     <BrowserRouter>
+         <Routes>
+             <Route path='dashboard' element={<Dashboard/>} />
+             <Route path='bookings' element={<Bookings/>} />
+             <Route path='cabins' element={<Cabins/>} />
+             <Route path='users' element={<Users/>} />
+             <Route path='setting' element={<Settings/>} />
+             <Route path='account' element={<Account/>} />
+             <Route path='login' element={<Login/>} />
+             <Route path='*' element={<PageNotFound/>} />
+         </Routes>
+     </BrowserRouter>
+    );
+};
 
-const H1= styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-`
-
-function App() {
-  return(
-      <>
-    <GlobalStyles/>
-  <div>
-    <Heading>the Wild Oasis</Heading>
-  </div>
-  </>
-  )
-}
-
-export default App
+export default App;
