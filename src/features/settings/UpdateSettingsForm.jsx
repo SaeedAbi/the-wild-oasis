ed065a22de0a01516e7b44b07fdb459485327772
SaeedAbi <abdary.saeed@gmail.com>
2024-07-04 14:27:50 +0330

    :zap: add UPDATE for settings

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -3,25 +3,35 @@ import FormRow from '../../ui/FormRow';
 import Input from '../../ui/Input';
 import {useSetting} from "../../hooks/useSetting.js";
 import Spinner from "../../ui/Spinner.jsx";
+import {useEditSetting} from "../../hooks/useEditSetting.js";
 
 function UpdateSettingsForm() {
     const {isLoading,error,settings:{minBookingLength,maxBookingLength,maxGuestPerBooking,breakfastPrice}={}}=useSetting()
+    const {isEditing,updateSetting}=useEditSetting()
+
 
     if (isLoading) return <Spinner/>
+
+    function handleUpdate(e,field){
+        const {value}=e.target
+
+        if (!value) return
+        updateSetting({[field]:value})
+    }
 
   return (
     <Form>
       <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' defaultValue={minBookingLength}/>
+        <Input disabled={isEditing} type='number' id='min-nights' defaultValue={minBookingLength} onBlur={e=>handleUpdate(e,'minBookingLength')}/>
       </FormRow>
       <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' defaultValue={maxBookingLength}/>
+        <Input type='number' id='max-nights' defaultValue={maxBookingLength} onBlur={e=>handleUpdate(e,'maxBookingLength')}/>
       </FormRow>
       <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' defaultValue={maxGuestPerBooking}/>
+        <Input type='number' id='max-guests' defaultValue={maxGuestPerBooking} onBlur={e=>handleUpdate(e,'maxGuestPerBooking')}/>
       </FormRow>
       <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' defaultValue={breakfastPrice}/>
+        <Input type='number' id='breakfast-price' defaultValue={breakfastPrice} onBlur={e=>handleUpdate(e,'breakfastPrice')}/>
       </FormRow>
     </Form>
   );
