e911325020ebbb50ea934f4b7349e416215d6f39
SaeedAbi <abdary.saeed@gmail.com>
2024-07-07 19:48:32 +0330

    :construction: add Pagination component

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -4,6 +4,7 @@ import Menus from "../../ui/Menus";
 import Empty from "../../ui/Empty.jsx";
 import {useBookings} from "../../hooks/useBookings.js";
 import Spinner from "../../ui/Spinner.jsx";
+import Pagination from "../../ui/Pagination.jsx";
 
 function BookingTable() {
   const {bookings,isLoading}=useBookings()
@@ -29,6 +30,9 @@ function BookingTable() {
             <BookingRow key={booking.id} booking={booking} />
           )}
         />
+        <Table.Footer>
+          <Pagination count={15}/>
+        </Table.Footer>
       </Table>
     </Menus>
   );
