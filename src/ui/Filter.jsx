0e8113626a047bd81cc96e04464fee2ced25d026
SaeedAbi <abdary.saeed@gmail.com>
2024-07-07 15:21:32 +0330

    :zap: add disabled to filter button

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -45,7 +45,7 @@ searchParams.set(filterField,value)
   }
   return (
       <StyledFilter>
-        {options.map(option=>(<FilterButton active={option.value===currentFilter} key={option.value} onClick={()=>handleClick(option.value)}>{option.label}</FilterButton>)
+        {options.map(option=>(<FilterButton active={option.value===currentFilter} disabled={option.value===currentFilter} key={option.value} onClick={()=>handleClick(option.value)}>{option.label}</FilterButton>)
             )}
       </StyledFilter>
   );
