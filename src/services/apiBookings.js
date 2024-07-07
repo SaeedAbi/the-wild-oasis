146f3f8fef97ad07f86c9842af091a06c9906622
SaeedAbi <abdary.saeed@gmail.com>
2024-07-07 18:42:25 +0330

    :zap: add filter too Bookings component

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,9 +1,16 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase.js";
+import {fi} from "date-fns/locale";
 
-export async function getBookings(){
-  const {data,error}=await supabase.from("bookings").select("id,created_at,startDate,endDate,numNights,numGuests,status,totalPrice,cabins(name),guests(fullName,email)")
+export async function getBookings({filter,sortBy}){
+  let query=supabase.from("bookings").select(
+      "id,created_at,startDate,endDate,numNights,numGuests,status,totalPrice,cabins(name),guests(fullName,email)")
 
+  if (filter !== null) query=query[filter.method || 'eq'](filter.field,filter.value)
+
+
+
+const {data,error}=await query
   if (error){
     console.error(error)
     throw new Error('Bookings could not be loaded')
