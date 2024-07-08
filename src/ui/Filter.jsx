86404c4ea8a1e56c153975ee295ea5cbcbebd4bc
SaeedAbi <abdary.saeed@gmail.com>
2024-07-08 17:54:57 +0330

    :bug: fixing pagination when change filter in other pages

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -41,6 +41,7 @@ const [searchParams,setSearchParams]=useSearchParams()
 
   function handleClick(value){
 searchParams.set(filterField,value)
+      if (searchParams.get('page')) searchParams.set('page',1)
     setSearchParams(searchParams)
   }
   return (
