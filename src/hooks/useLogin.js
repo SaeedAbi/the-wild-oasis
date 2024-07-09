aacb534c219099a045abd94b9fdffce3bc287603
SaeedAbi <abdary.saeed@gmail.com>
2024-07-09 16:46:10 +0330

    :bug: add setQueryData to useLogin

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,14 +1,16 @@
-import {useMutation} from "@tanstack/react-query";
+import {useMutation, useQueryClient} from "@tanstack/react-query";
 import {login as loginApi} from "../services/apiAuth.js";
 import {useNavigate} from "react-router-dom";
 import toast from "react-hot-toast";
 
 export function useLogin(){
+    const queryClinet=useQueryClient()
     const navigate=useNavigate()
 
     const {mutate:login,isLoading} =useMutation({
         mutationFn:({email,password})=>loginApi({email,password}),
         onSuccess:(user)=>{
+            queryClinet.setQueryData(['user'],user.user)
             navigate('/dashboard',{replace:true})
         },
         onError:err=>{
