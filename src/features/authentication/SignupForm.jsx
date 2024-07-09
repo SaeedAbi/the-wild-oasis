89dab8f1cbdb60f0ce5abf0e360de9793179fbd8
SaeedAbi <abdary.saeed@gmail.com>
2024-07-09 17:35:03 +0330

    :zap: add sign up for users

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -3,45 +3,50 @@ import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 import {useForm} from "react-hook-form";
+import {signup} from "../../services/apiAuth.js";
+import {useSignup} from "../../hooks/useSignup.js";
 
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
     const{register,formState,getValues,handleSubmit}=useForm()
-    const {errors}=formState
+    const {errors,reset}=formState
+    const {signup,isLoading}=useSignup()
 
-    function onSubmit(data){
-        console.log(data)
+    function onSubmit({fullName,email,password}){
+        signup({fullName, email, password},{
+            onSettled:()=> reset(),
+        })
     }
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
       <FormRow label="Full name" error={errors?.fullName?.message}>
-        <Input type="text" id="fullName" {...register('fullName',{required:'This field is required'})} />
+        <Input type="text" disabled={isLoading} id="fullName" {...register('fullName',{required:'This field is required'})} />
       </FormRow>
 
       <FormRow label="Email address" error={errors?.email?.message}>
-        <Input type="email" id="email" {...register('email',{required:'This field is required',pattern:{
+        <Input type="email" disabled={isLoading} id="email" {...register('email',{required:'This field is required',pattern:{
             value:/\S+@\S+\.\S+/,
                 message:'please provide a valid email'
             }})} />
       </FormRow>
 
       <FormRow label="Password (min 8 characters)" error={errors?.password?.message}>
-        <Input type="password" id="password" {...register('password',{required:'This field is required',minLength:{
+        <Input type="password" disabled={isLoading} id="password" {...register('password',{required:'This field is required',minLength:{
             value:8,
                 message:'password needs a minimum of 8 characters'
             }})}/>
       </FormRow>
 
       <FormRow label="Repeat password" error={errors?.passwordConfirm?.message}>
-        <Input type="password" id="passwordConfirm" {...register('passwordConfirm',{required:'This field is required',
+        <Input type="password" disabled={isLoading} id="passwordConfirm" {...register('passwordConfirm',{required:'This field is required',
             validate:(value)=>value===getValues().password || 'Passwords need to match',})}/>
       </FormRow>
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
+        <Button variation="secondary" type="reset" disabled={isLoading}>
           Cancel
         </Button>
         <Button>Create new user</Button>
--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,5 +1,19 @@
 import supabase from "./supabase.js";
 
+export async function signup({fullName,email,password}){
+     const{data,error}= await supabase.auth.signUp({
+        email,password,options:{
+            data:{
+                fullName,
+                avatar:'',
+            }
+        }
+    })
+    if (error) throw new Error(error.message)
+
+    return data
+}
+
 export async function login({email,password}){
 
     let { data, error } = await supabase.auth.signInWithPassword({
@@ -28,3 +42,4 @@ export async function logout(){
 
     if (error) throw new Error(error.message)
 }
+
