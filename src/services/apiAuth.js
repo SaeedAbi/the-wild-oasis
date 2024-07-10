bb90b212abe9ec74473f2cf5c4555b6a4ce96f52
SaeedAbi <abdary.saeed@gmail.com>
2024-07-10 15:01:28 +0330

    :zap: add UpdateUserDataForm component to edit user data

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase.js";
+import supabase, {supabaseUrl} from "./supabase.js";
 
 export async function signup({fullName,email,password}){
      const{data,error}= await supabase.auth.signUp({
@@ -43,3 +43,22 @@ export async function logout(){
     if (error) throw new Error(error.message)
 }
 
+export async function updateCurrentUser({password,fullName,avatar}){
+    let updataData
+    if (password) updataData = {password}
+    if (fullName) updataData={data: {fullName}}
+
+    const {data,error}=  await supabase.auth.updateUser(updataData)
+    if (error) throw new Error(error.message)
+    if (!avatar) return data
+
+    const fileName=`avatar-${data.user.id}-${Math.random()}`
+    const {error:storageError}=await supabase.storage.from('avatars').upload(fileName,avatar)
+    if (storageError) throw new Error(storageError.message)
+
+    const {data:updatedUser,error:error2}=supabase.auth.updateUser({data:{
+        avatar:`${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`
+        }})
+    if (error2) throw new Error(error2.message)
+    return updatedUser
+}
